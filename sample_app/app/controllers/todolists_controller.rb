f23968032b73e37f48c5965187f9a6d29029faa6
EC2 Default User <ec2-user@ip-172-31-75-207.ec2.internal>
2021-02-16 11:44:18 +0000

    [Finish] 2-8

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -3,7 +3,7 @@ class TodolistsController < ApplicationController
     # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
     @list = List.new
   end
-  
+
   def create
     # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -12,28 +12,34 @@ class TodolistsController < ApplicationController
     # ３. トップ画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
   end
-  
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
 
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+  end
+
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4b7bcd9a53abb0fa80929c86e63dbf9372e78acea8e09890ed9373645db9ade46ddb37287574478387aa4209867c71fcd58078ae3c221945066c4324bfd3ae99'
